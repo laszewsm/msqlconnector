b5fd4e731c52bb0f16de5575935f8805e76ff645
laszewsm <mariusz.laszewski@nerdokracja.pl>
2015-03-02 12:57:55 +0000

    poprawione definicjie
    Poprawiłem defnicje
    teraz trzeba rezsłupłac function i strip

--- okno.py
+++ okno.py
@@ -4,55 +4,63 @@ from Tkinter import *
 import MySQLdb  # TO BEDZIE TRZEBA USUNAC
 import mysql.connector
 from mysql.connector import errorcode
-#definicje
+import urllib
+import urllib2
+import htmllib
+# definicje
+
 
 root = Tk()
 
 
-
 #rozmier okna
-RTitle=root.title("MySQL connector") #Nazwa okna
-root.minsize(600,450)   #rozmiar okna
-root.maxsize(600,450)
+RTitle = root.title("MySQL connector")  #Nazwa okna
+root.minsize(600, 450)  #rozmiar okna
+root.maxsize(600, 450)
 
 
 
-# USER
+def usr(self): pass
 
 user = Label(root, text="user name")
-user.pack( side = TOP, anchor = W)
-E1 = Entry(root, bd =5)
-E1.pack(side = TOP, anchor = W)
+user.pack(side=TOP, anchor=W)
+uEntry = Entry(root, bd=5).pack(side=TOP, anchor=W)
+#u = Entry(root, bd =5)
+#u.pack(side = TOP, anchor = W,)
+
 
 # PASSWORD
+def paswd(self): pass
+
 
 password = Label(root, text="password")
-password.pack( side = TOP, anchor = W)
-E2 = Entry(root, bd =5)
-E2.pack(side = TOP, anchor = W)
+password.pack(side=TOP, anchor=W)
+pEntry = Entry(root, bd=5).pack(side=TOP, anchor=W)
+#   p = Entry(root, bd =5)
+#p.pack(side = TOP, anchor = W)
 
 # HOST
+def hst(self): pass
+
 
 host = Label(root, text="host")
-host.pack( side = TOP, anchor = W)
-E3 = Entry(root, bd =5)
-E3.pack(side = TOP, anchor = W)
- # BASE
+host.pack(side=TOP, anchor=W)
+hEntry = Entry(root, bd=5).pack(side=TOP, anchor=W)
+#h = Entry(root, bd =5)
+#h.pack(side = TOP, anchor = W)
+# BASE
+def bsd(self): pass
+
 
 base = Label(root, text="base")
-base.pack( side = TOP, anchor = W)
-E4 = Entry(root, bd =5)
-E4.pack(side = TOP, anchor = W)
-
-
-
-
-
-#################### Dfinicja dla przycisku RUN
+base.pack(side=TOP, anchor=W)
+bEntry = Entry(root, bd=5).pack(side=TOP, anchor=W)
+#b = Entry(root, bd =5)
+#b.pack(side = TOP, anchor = W)  #################### Dfinicja dla przycisku RUN
 
 def run():
     try:
-        cnx = mysql.connector.connect(user="E1",password="E2",host="E3", database="E4")
+        cnx = mysql.connector.connect(user= usr, password= paswd, host= hst, database= bsd)
     except mysql.connector.Error as err:
         if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
             print("Cos poszlo nie tak, sprawdz wprowadzone dane, albo zjedz banana")
@@ -62,7 +70,6 @@ def run():
             print(err)
     else:
         cnx.close()
-
 
 
 ##### Przycisk RUN
@@ -77,11 +84,11 @@ root.mainloop()
 
 #db = MySQLdb.connect(user = E1, password = E2, host = E3, db = E4)
 
-  #  cursor = conn.cursor ()
-  #  cursor.execute ("SELECT VERSION()")
-  #  row = cursor.fetchone ()
-  #  print "server version:", row[0]
-  #  cursor.close ()
+#  cursor = conn.cursor ()
+#  cursor.execute ("SELECT VERSION()")
+#  row = cursor.fetchone ()
+#  print "server version:", row[0]
+#  cursor.close ()
 
 
 
